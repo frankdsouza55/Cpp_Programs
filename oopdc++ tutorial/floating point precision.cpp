b9c7f4e97b733245267d342e7a67e34495c743f4
frankdsouza55 <frankdsouza55@gmail.com>
2017-02-14 16:45:54 +0530

    Update floating point precision.cpp

--- oopdc++ tutorial/floating point precision.cpp
+++ oopdc++ tutorial/floating point precision.cpp
@@ -6,13 +6,13 @@ int main()
 {
     double root2 = sqrt(2);
     int places;
-    cout<<"\nSquate root of 2 with precision 0 - 9 \n";
+    cout<<"\nSquare root of 2 with precision 0 - 9 \n";
     for(places = 0; places<10; places++)
     {
         cout.precision(places);
         cout<<root2<<endl;
     }
-    cout<<"\nSquate root of 2 with precision 0 - 9 using setprecision()\n";
+    cout<<"\nSquare root of 2 with precision 0 - 9 using setprecision()\n";
     for(places = 0; places<10; places++)
         cout<<setprecision(places)<<root2<<endl;
     return 0;
